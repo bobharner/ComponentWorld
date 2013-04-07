3abd41d720693ca2b5797b0c616a465a1061f4e2
Bob Harner <bobharner@apache.org>
2013-04-07 17:08:31 -0400

    entryFilter.js (RequireJS module) needed for Tapestry 5.4

---
+++ src/main/resources/META-INF/modules/entryFilter.js
@@ -0,0 +1,19 @@
+// A Require.js module that handles events related to filtering the entries on
+// the home page
+
+define(["t5/core/dom"], function(t5) {
+	return function(selectClientId) {
+		// event handler for "entry type" selection menu
+		t5(selectClientId).on('change', function(e) {
+			console.log("found entryType menu changed");
+			var form = this.findParent("form");
+			return form.trigger("submit");
+		});
+		// event handler for "source type" radio buttons
+		t5("#checkall").on('change', function(e) {
+			console.log("found sourceType radio changed");
+			var form = this.findParent("form");
+			return form.trigger("submit");
+		});
+	};
+});
