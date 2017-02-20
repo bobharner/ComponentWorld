3a3e32144ec126e9a22fc2b15fa14c77c7507b6c
Bob Harner <bobharner@gmail.com>
2017-02-20 16:58:59 -0500

    Added "Administration" to breadcrumb links for admin pages

--- src/main/java/org/apache/componentworld/components/Layout.java
+++ src/main/java/org/apache/componentworld/components/Layout.java
@@ -14,10 +14,13 @@
  */
 package org.apache.componentworld.components;
 
-import org.apache.tapestry5.*;
-import org.apache.tapestry5.annotations.*;
-import org.apache.tapestry5.ioc.annotations.*;
 import org.apache.tapestry5.BindingConstants;
+import org.apache.tapestry5.Block;
+import org.apache.tapestry5.ComponentResources;
+import org.apache.tapestry5.annotations.Import;
+import org.apache.tapestry5.annotations.Parameter;
+import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.ioc.annotations.Inject;
 
 /**
  * Layout component for most or all pages of application.
@@ -31,33 +34,31 @@ public class Layout
 	private ComponentResources resources;
 
 	/** The page title, for the <title> element and the <h1>element. */
-	@SuppressWarnings("unused")
 	@Property
 	@Parameter(required = true, defaultPrefix = BindingConstants.LITERAL)
 	private String title;
 
-	/**Optional CSS rules to place into the page head */
-	@SuppressWarnings("unused")
+	/** Optional CSS rules to place into the page head */
 	@Property
 	@Parameter(defaultPrefix = BindingConstants.LITERAL)
 	private Block style;
 
-	@SuppressWarnings("unused")
 	@Property
 	private String pageName;
 
-    @SuppressWarnings("unused")
     @Property
     @Parameter(required = false, defaultPrefix = BindingConstants.LITERAL)
     private String section;
 
-    /**
-     * A block that displays in the upper-right corner of the page
-     */
-    @SuppressWarnings("unused")
+    /** A block that displays in the upper-right corner of the page */
     @Property
     @Parameter(required = false, defaultPrefix = BindingConstants.PROP)
     private Block tools;
+
+    /** A block of additional links to display in the breadcrumbs */
+    @Property
+    @Parameter(required = false, defaultPrefix = BindingConstants.PROP)
+    private Block pageLinks;
 
 	void SetupRender()
 	{
